cd673f24c6c1478c8e7ef432aa341ee31a8e7149
timbophillips <timothy.john.phillips@gmail.com>
2020-10-13 20:55:10 +0800

    what a blast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FilteredSelectModule } from 'filtered-select';
-import { FilterSelect2Component } from './filter-select2/filter-select2.component';
 
 @NgModule({
-  declarations: [AppComponent, FilterSelect2Component],
+  declarations: [AppComponent],
   imports: [BrowserModule, GraphQLModule, HttpClientModule, FilteredSelectModule],
   providers: [],
   bootstrap: [AppComponent]
